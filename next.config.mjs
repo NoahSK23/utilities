6c28317a7477b77b92e0c1f83150fb1e18066304
Noah Klinger <ipie1000@gmail.com>
2024-12-22 00:43:52 -0500

    Added basename for gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/utilities',
+  assetPrefix: '/utilities',
   typescript: {
     ignoreBuildErrors: true,
   },
